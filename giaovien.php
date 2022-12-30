80f3c8f34f34de9708efc7a7036e80107c935ee7
hailm <hai.lemanh.1310@gmail.com>
2022-12-30 17:52:31 +0700

    giaovien

--- giaovien.php
+++ giaovien.php
@@ -25,11 +25,41 @@
 
 <div class="uk-background-muted uk-section-xsmall">
     <div class="uk-container">
-        <div class="uk-grid uk-grid-small" uk-grid>
+        <div class="uk-grid uk-grid-small uk-grid-30-m" uk-grid>
             <div class="uk-width-expand">
-                <div class="uk-height-viewport">
+                <div class="home__item-70-40">
 
                 </div>
+
+                <!--Câu hỏi thường gặp về giáo viên-->
+                <div class="home__item-70-40">
+                    <div class="home__item-40-16 uk-text-center">
+                        <h2 class="uk-h2 home__title">Câu hỏi thường gặp về giáo viên</h2>
+                    </div>
+                    <div class="home__item-40-16">
+                        <ul class="uk-accordion" uk-accordion>
+                            <li class="uk-open">
+                                <a class="uk-accordion-title" href="#">Item 1</a>
+                                <div class="uk-accordion-content">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                </div>
+                            </li>
+                            <li>
+                                <a class="uk-accordion-title" href="#">Item 2</a>
+                                <div class="uk-accordion-content">
+                                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor reprehenderit.</p>
+                                </div>
+                            </li>
+                            <li>
+                                <a class="uk-accordion-title" href="#">Item 3</a>
+                                <div class="uk-accordion-content">
+                                    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat proident.</p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!--/Câu hỏi thường gặp về giáo viên-->
             </div>
             <div class="uk-width-1-3@m">
                 <aside class="sidebar">
