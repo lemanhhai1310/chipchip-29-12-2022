68980d7e3e82f7cf1178b3427cfbd5a5eebc3a0a
hailm <hai.lemanh.1310@gmail.com>
2023-01-03 19:20:37 +0700

    update

--- giaovien.php
+++ giaovien.php
@@ -28,48 +28,75 @@
     <div class="uk-container">
         <div class="uk-grid uk-child-width-auto uk-grid-10" uk-grid>
             <div>
-                <div uk-form-custom="target: > * > span:first-child">
-                    <select aria-label="Custom controls">
-                        <option value="">Giáo viên</option>
-                        <option value="1">Option 01</option>
-                        <option value="2">Option 02</option>
-                        <option value="3">Option 03</option>
-                        <option value="4">Option 04</option>
-                    </select>
+                <div class="uk-inline">
                     <button class="uk-button giaovien__filter__btn uk-button-default uk-border-pill" type="button" tabindex="-1">
-                        <span></span>
+                        <span>Giáo viên</span>
                         <span uk-icon="icon: chevron-down"></span>
                     </button>
+                    <div class="uk-width-large" uk-dropdown="mode: click">
+                        <div class="uk-margin">
+                            <h5 class="uk-h5 giaovien__filter__txt uk-margin-remove">Checkbox</h5>
+                            <div class="uk-grid-10 item__10 uk-child-width-auto uk-grid">
+                                <label><input class="uk-checkbox giaovien__filter__checkbox" type="checkbox" checked> <span class="giaovien__filter__box">A</span></label>
+                                <label><input class="uk-checkbox giaovien__filter__checkbox" type="checkbox"> <span class="giaovien__filter__box">B</span></label>
+                            </div>
+                        </div>
+                        <div class="uk-margin">
+                            <h5 class="uk-h5 giaovien__filter__txt uk-margin-remove">Radio</h5>
+                            <div class="uk-grid-10 item__10 uk-child-width-auto uk-grid">
+                                <label><input class="uk-radio giaovien__filter__radio" type="radio" name="radio2" checked> <span class="giaovien__filter__box">A</span></label>
+                                <label><input class="uk-radio giaovien__filter__radio" type="radio" name="radio2"> <span class="giaovien__filter__box">B</span></label>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div>
-                <div uk-form-custom="target: > * > span:first-child">
-                    <select aria-label="Custom controls">
-                        <option value="">Mục tiêu học tập</option>
-                        <option value="1">Option 01</option>
-                        <option value="2">Option 02</option>
-                        <option value="3">Option 03</option>
-                        <option value="4">Option 04</option>
-                    </select>
+                <div class="uk-inline">
                     <button class="uk-button giaovien__filter__btn uk-button-default uk-border-pill" type="button" tabindex="-1">
-                        <span></span>
+                        <span>Mục tiêu học tập</span>
                         <span uk-icon="icon: chevron-down"></span>
                     </button>
+                    <div class="uk-width-large" uk-dropdown="mode: click">
+                        <div class="uk-margin">
+                            <h5 class="uk-h5 giaovien__filter__txt uk-margin-remove">Checkbox</h5>
+                            <div class="uk-grid-10 item__10 uk-child-width-auto uk-grid">
+                                <label><input class="uk-checkbox giaovien__filter__checkbox" type="checkbox" checked> <span class="giaovien__filter__box">A</span></label>
+                                <label><input class="uk-checkbox giaovien__filter__checkbox" type="checkbox"> <span class="giaovien__filter__box">B</span></label>
+                            </div>
+                        </div>
+                        <div class="uk-margin">
+                            <h5 class="uk-h5 giaovien__filter__txt uk-margin-remove">Radio</h5>
+                            <div class="uk-grid-10 item__10 uk-child-width-auto uk-grid">
+                                <label><input class="uk-radio giaovien__filter__radio" type="radio" name="radio2" checked> <span class="giaovien__filter__box">A</span></label>
+                                <label><input class="uk-radio giaovien__filter__radio" type="radio" name="radio2"> <span class="giaovien__filter__box">B</span></label>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div>
-                <div uk-form-custom="target: > * > span:first-child">
-                    <select aria-label="Custom controls">
-                        <option value="">Trình độ của con</option>
-                        <option value="1">Option 01</option>
-                        <option value="2">Option 02</option>
-                        <option value="3">Option 03</option>
-                        <option value="4">Option 04</option>
-                    </select>
+                <div class="uk-inline">
                     <button class="uk-button giaovien__filter__btn uk-button-default uk-border-pill" type="button" tabindex="-1">
-                        <span></span>
+                        <span>Trình độ của con</span>
                         <span uk-icon="icon: chevron-down"></span>
                     </button>
+                    <div class="uk-width-large" uk-dropdown="mode: click">
+                        <div class="uk-margin">
+                            <h5 class="uk-h5 giaovien__filter__txt uk-margin-remove">Checkbox</h5>
+                            <div class="uk-grid-10 item__10 uk-child-width-auto uk-grid">
+                                <label><input class="uk-checkbox giaovien__filter__checkbox" type="checkbox" checked> <span class="giaovien__filter__box">A</span></label>
+                                <label><input class="uk-checkbox giaovien__filter__checkbox" type="checkbox"> <span class="giaovien__filter__box">B</span></label>
+                            </div>
+                        </div>
+                        <div class="uk-margin">
+                            <h5 class="uk-h5 giaovien__filter__txt uk-margin-remove">Radio</h5>
+                            <div class="uk-grid-10 item__10 uk-child-width-auto uk-grid">
+                                <label><input class="uk-radio giaovien__filter__radio" type="radio" name="radio2" checked> <span class="giaovien__filter__box">A</span></label>
+                                <label><input class="uk-radio giaovien__filter__radio" type="radio" name="radio2"> <span class="giaovien__filter__box">B</span></label>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -81,6 +108,68 @@
     <div class="uk-container">
         <div class="uk-grid uk-grid-small" uk-grid>
             <div class="uk-width-expand">
+                <div class="home__item-70-40">
+                    <h3 class="uk-h3 uk-margin-remove giaovien__list__title">Giáo viên đã học</h3>
+                    <?php for ($i=1;$i<=4;$i++): ?>
+                        <div class="uk-card giaovien__list__card item__15 uk-card-default uk-card-body uk-padding-small uk-border-rounded">
+                            <div class="uk-grid uk-grid-small" uk-grid>
+                                <div class="uk-width-auto uk-text-center">
+                                    <div class="uk-cover-container giaovien__list__coverImg">
+                                        <img src="images/noimage.jpg" alt="" uk-cover="">
+                                        <canvas width="122" height="115"></canvas>
+                                    </div>
+                                    <div class="uk-visible@m item__10 home__txt">5687 lớp học</div>
+                                </div>
+                                <div class="uk-width-expand">
+                                    <div class="uk-grid uk-grid-small" uk-grid>
+                                        <div class="uk-width-expand">
+                                            <div class="uk-grid uk-grid-5 uk-child-width-auto uk-flex-middle" uk-grid>
+                                                <div>
+                                                    <h5 class="uk-h5 home__giaovien__card__name"><a href="">Thầy Edward</a></h5>
+                                                </div>
+                                                <?php if ($i%2!=0): ?>
+                                                    <div>
+                                                        <span class="home__giaovien__card__tag uk-label uk-label-warning uk-border-pill uk-text-capitalize">Super Teacher</span>
+                                                    </div>
+                                                <?php endif; ?>
+                                            </div>
+                                        </div>
+                                        <div class="uk-width-auto">
+                                            <div class="uk-grid uk-child-width-auto uk-grid-5 uk-flex-middle" uk-grid>
+                                                <div class="home__giaovien__card__star">
+                                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                                    <span class="home__giaovien__card__star__count">5.0</span>
+                                                </div>
+                                                <div>
+                                                    <div class="home__giaovien__card__txt1">(67 đánh giá)</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <?php if ($i<=2): ?>
+                                        <div class="item__5">
+                                            <span class="uk-label uk-label-success giaovien__list__label">Bản ngữ</span>
+                                        </div>
+                                    <?php endif; ?>
+
+                                    <div class="item__10">
+                                        <div class="uk-hidden@m home__giaovien__card__txt uk-flex uk-flex-row-reverse uk-flex-middle" uk-icon="icon: user; ratio:0.8">
+                                            <span class="uk-flex-auto uk-text-middle">80 học sinh * 5603 bài học</span>
+                                        </div>
+                                        <div class="uk-visible@m">
+                                            <?php require "template-parts/block01.php"; ?>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="uk-width-1-1 uk-hidden@m">
+                                    <?php require "template-parts/block01.php"; ?>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endfor; ?>
+                </div>
+
+
                 <div class="home__item-70-40">
                     <?php for ($i=1;$i<=4;$i++): ?>
                     <div class="uk-card giaovien__list__card item__15 uk-card-default uk-card-body uk-padding-small uk-border-rounded">
@@ -120,7 +209,7 @@
                                 </div>
                                 <?php if ($i<=2): ?>
                                 <div class="item__5">
-                                    <span class="uk-label uk-label-success">Bản ngữ</span>
+                                    <span class="uk-label uk-label-success giaovien__list__label">Bản ngữ</span>
                                 </div>
                                 <?php endif; ?>
 
