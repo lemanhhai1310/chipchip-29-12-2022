bd763920b4fd2442ff80dbed8a1cd0acf8775ea8
hailm <hai.lemanh.1310@gmail.com>
2023-01-06 09:51:18 +0700

    fix

--- lophoc.php
+++ lophoc.php
@@ -4,7 +4,7 @@
 <?php $lophoc = '' ?>
 <?php require "template-parts/layouts/header.php"; ?>
 
-<div class="uk-background-muted uk-section-xsmall uk-height-min-large">
+<div class="uk-background-muted uk-section-xsmall uk-height-min-large" uk-height-viewport="offset-top: true">
     <div class="uk-container">
         <h3 class="uk-h3 uk-margin-remove giaovien__list__title">Lớp học của con</h3>
         <div class="uk-grid uk-margin uk-child-width-1-4@m uk-grid-small" uk-grid>
