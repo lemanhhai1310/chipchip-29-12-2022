3f804cc0d9b5ea507b240ce30e60974c6202c40f
hailm <hai.lemanh.1310@gmail.com>
2023-02-09 08:18:34 +0700

    update

---
+++ page3.php
@@ -0,0 +1,14 @@
+<?php $data["title"] = "Page 3"; ?>
+<?php $bodyClass = '' ?>
+<?php $is_from_login = true; ?>
+<?php $lophoc = '' ?>
+<?php require "template-parts/layouts/header.php"; ?>
+<div class="uk-background-default uk-section-xsmall uk-height-viewport">
+    <div class="uk-container">
+        <label>
+            <input class="uk-checkbox page3__checkbox" type="checkbox" checked>
+            <span class="page3__labelBox uk-border-pill"></span>
+        </label>
+    </div>
+</div>
+<?php require "template-parts/layouts/footer.php"; ?>
